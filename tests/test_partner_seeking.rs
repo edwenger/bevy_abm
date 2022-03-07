2d28135379d6cfc112809c61b85c0ef36b6848cd
Edward Wenger <wenger.edward@gmail.com>
2022-03-06 19:03:51 -0800

    update tests to event changes

--- tests/test_partner_seeking.rs
+++ tests/test_partner_seeking.rs
@@ -2,7 +2,6 @@
 extern crate approx;
 
 use bevy::prelude::*;
-use bevy::app::Events;
 
 use bevy_abm::individual::*;
 
@@ -11,8 +10,6 @@ fn did_start_partner_seeking() {
 
     // Setup world
     let mut world = World::default();
-    world.insert_resource(Events::<DeathEvent>::default());
-    world.insert_resource(Events::<BecomeAdultEvent>::default());
 
     // Setup stage with aging system
     let mut update_stage = SystemStage::parallel();
@@ -20,16 +17,21 @@ fn did_start_partner_seeking() {
     update_stage.add_system(start_partner_seeking.after("aging"));
 
     // Setup test entities
-    let individual_id = world.spawn().insert(Individual).insert(Demog { age: 15.0, sex: Sex::Male }).id();
+    let individual_id = world.spawn().insert(Individual).insert(Demog{ age: 19.5, sex: Sex::Male}).id();
+
+    // Check before state
+    assert!(world.get::<Individual>(individual_id).is_some());
+    relative_eq!(world.get::<Demog>(individual_id).unwrap().age, 19.5, epsilon = f32::EPSILON);
+    assert!(world.get::<Adult>(individual_id).is_none());
+    assert!(world.get::<PartnerSeeking>(individual_id).is_none());
 
     // Run systems
-    for _tstep in 0..12 {
+    for _tstep in 0..12 {  // dependent on AGING_TIMESTEP
         update_stage.run(&mut world);
     }
 
     // Check resulting changes
-    assert!(world.get::<Individual>(individual_id).is_some());
-    relative_eq!(world.get::<Demog>(individual_id).unwrap().age, 16.0, epsilon = f32::EPSILON);
-    // assert!(world.get::<PartnerSeeking>(individual_id).is_some());
-
+    relative_eq!(world.get::<Demog>(individual_id).unwrap().age, 20.5, epsilon = f32::EPSILON);
+    assert!(world.get::<Adult>(individual_id).is_some());
+    assert!(world.get::<PartnerSeeking>(individual_id).is_some());
 }
